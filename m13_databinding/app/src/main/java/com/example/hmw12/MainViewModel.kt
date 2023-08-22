e94ac1ec674edd2137e23e78a0ad987658e2e78b
Alexey Kochkov <avik_mt-302@mail.ru>
2023-08-22 14:24:55 +0300

    hmw13 done

---
+++ m13_databinding/app/src/main/java/com/example/hmw12/MainViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.hmw12
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.Observable
+
+class MainViewModel : ViewModel() {
+    private val state_ = MutableStateFlow<State>(State.Success(""))
+    val state = state_.asStateFlow()
+    val observer = Observer()
+    var jobFindText: Job? = null
+
+    fun findText(text: String) {
+//        jobFindText?.cancel()
+        if (text.length <= 3) {
+            jobFindText?.cancel()
+            state_.value = State.Success("должно быть больше 3 символов")
+            return
+        }
+        if (jobFindText != null) {
+            jobFindText!!.cancel()
+        }
+        jobFindText = viewModelScope.launch {
+            delay(3_000)
+            state_.value = State.Loading
+            delay(5_000)
+            state_.value = State.Success("по запросу \"${text}\" ничего не найдено")
+        }
+    }
+
+    inner class Observer: BaseObservable () {
+        @Bindable
+        var text: String = ""
+        set(value) {
+            field = value
+            findText(value)
+            notifyPropertyChanged(BR.text)
+        }
+    }
+}
