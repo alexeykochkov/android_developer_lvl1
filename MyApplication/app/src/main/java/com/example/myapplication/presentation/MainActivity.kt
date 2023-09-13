fcc2616dc3e1e80eca73bf27fe7ce65b7fd87380
alexey_kochkov <alexeykochkov@gmail.com>
2023-09-13 23:29:09 +0300

    new f*cked changes

--- MyApplication/app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.myapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.example.myapplication.R
+import com.example.myapplication.data.RetrofitInstance
+import com.example.myapplication.data.UsefulActivitiesRepository
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.domain.GetUsefulActivityUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val viewModel: MainViewModel by viewModels{MainViewModel.Factory}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.refresh.setOnClickListener {
+            viewModel.reloadUsefulActivity()
+        }
+
+
+//        val view = MainViewModel(gto)
+//        view.reloadUsefulActivity()
+
+//        val gto = GetUsefulActivityUseCase(rerpository = UsefulActivitiesRepository(RetrofitInstance.usefulActivityRepository))
+//
+        //        val retrofit = RetrofitInstance.usefulActivityRepository
+//
+//        CoroutineScope(Dispatchers.IO).launch {
+//            try {
+//                var user = gto.execute()
+//                println("XUI ${user.activity}")
+//            } catch (e: Throwable) {
+//                e.message?.let { Log.e("My error", it) }
+//            }
+//        }
+
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/presentation/MainViewModel.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/presentation/MainViewModel.kt
@@ -1,7 +1,51 @@
 package com.example.myapplication.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.myapplication.data.RetrofitInstance
+import com.example.myapplication.data.UsefulActivitiesRepository
+import com.example.myapplication.domain.GetUsefulActivityUseCase
+import com.example.myapplication.entity.UsefulActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel(val case: GetUsefulActivityUseCase) : ViewModel() {
 
+    private val state_ = MutableStateFlow<UsefulActivity?>(null)
+
+    companion object {
+
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras
+            ): T {
+                val gto =
+                    GetUsefulActivityUseCase(rerpository = UsefulActivitiesRepository(
+                        RetrofitInstance.usefulActivityRepository)
+                    )
+                return MainViewModel(gto) as T
+            }
+        }
+    }
+
+    fun reloadUsefulActivity() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                println("IO")
+                state_.value = case.execute()
+
+            } catch (e: Throwable) {
+                e.message?.let { Log.e("My error", it) }
+            }
+        }
+    }
 }
